2b9a92d64688e56ab80774924c3fab7290ad13af
megasido <sidarerener@gmail.com>
2024-08-09 15:35:57 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 19, 69, 111),
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          color: Color.fromARGB(255, 19, 69, 111),
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          ),
+          titleTextStyle: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: StopWatchScreen(),
+    );
+  }
+}
